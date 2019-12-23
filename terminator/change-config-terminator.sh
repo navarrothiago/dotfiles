2c1bd69edfe241431e665e46f5b1939b87b0213b
Thiago Navarro <t_61327@cpqd.com.br>
2019-12-23 14:29:50 -0300

    Updates terminator script

--- terminator/change-config-terminator.sh
+++ terminator/change-config-terminator.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+TERMINATOR_DIR=/home/$USER/.config/terminator
 
 echo "Removendo arquivo config em /home/$USER/.config/terminator/config"
-rm /home/$USER/.config/terminator/config
+rm -r $TERMINATOR_DIR
 
 echo "Crinado link para o arquivo $1"
-ln -s  /work/dotfiles/terminator/$1 /home/$USER/.config/terminator/config
+#ln -s  /home/$USER/dotfiles/terminator/$1 /home/$USER/.config/terminator/config
+mkdir $TERMINATOR_DIR
+ln -s  /home/$USER/dotfiles/terminator/$1 $TERMINATOR_DIR/config
 
-ls -la /home/$USER/.config/terminator/config
+ls -la $TERMINATOR_DIR/config
  

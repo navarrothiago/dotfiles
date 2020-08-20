89c65c5a6f1f7e6639b4bd14af0e0bcd12001968
Thiago Navarro <navarro.ime@gmail.com>
2020-08-20 12:28:03 -0300

    Add touchscreen configuration
    Turn the extend screen (eDP1) to receive the input event from touchscreen device

---
+++ ipcam/README.md
@@ -0,0 +1,1 @@
+For more details: https://gist.github.com/navarrothiago/8402e3210989d69765bfb3115ccf0732
---
+++ x11/60-touchscreen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Put this script on /etc/X11/xinit/xinitrc.d/
+xinput --map-to-output 'CUST0000:00 27C6:0111' eDP1; xinput --map-to-output 'CUST0000:00 27C6:0111 Stylus' eDP1
